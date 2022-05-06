4ecdcf87708ca98e5bcb133372972856a2ac6d51
jjas-min <wtals@naver.com>
2022-05-06 10:46:58 +0900

    cafe.h struct수정 및 cafe.c 생성 및 1차수정

--- cafe.c
+++ cafe.c
@@ -1,10 +1,5 @@
-typedef struct{
-    char name[100];
-    char expl[100];
-    char type;
-    char taste;
-    int price;
-}Cafe;
+#include <stdio.h>
+#include "cafe.h"
 
 int loadFile(Cafe *c[]); //파일 불러오기 - 정승민
 void saveFile(Cafe *c[],int count); //파일 저장 - 정승민
@@ -13,7 +8,25 @@ void searchPrice(Cafe *c[],int count); //가격 검색 - 정지우
 void searchTaste(Cafe *c[],int count); //맛 검색 - 정지우
 void searchType(Cafe *c[],int count); //종류 검색 - 정지우
 int selectMenu(); //메뉴선택 - 정승민
-int createProduct(Cafe *c); //새로운 상품을 추가하는 함수 - 정승민
+int createProduct(Cafe *c){
+    printf("상품의 이름은? ");
+    getchar();
+    scanf("%[^\n]s",c->name);
+    getchar();
+    printf("상품의 설명은? ");
+    scanf("%[^\n]s",c->expl);
+    getchar();
+    printf("상품의 종류는? ");
+    scanf("%[^\n]s",c->type);
+    getchar();
+    printf("상품의 맛은? ");
+    getchar();
+    scanf("%[^\n]s",c->taste);
+    printf("상품의 가격은?");
+    getchar();
+    scanf("%d",&c->price);
+    return 1;
+} //새로운 상품을 추가하는 함수 - 정승민
 void readProduct(Cafe c); //상품 하나를 출력하는 함수 - 정승민
 int updateProduct(Cafe *c); //상품을 수정하는 함수 - 정승민
 void deleteProduct(Cafe *c[], int count); //상품 삭제하는 함수 - 정승민
