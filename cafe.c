d99b4c2d85b8e83f633611c99892ab1ed4882ab4
jjas-min <wtals@naver.com>
2022-05-08 22:29:18 +0900

    함수 테스트를 위한 메뉴 호출 기능 구현

--- cafe.c
+++ cafe.c
@@ -7,7 +7,22 @@ void searchName(Cafe *c[],int count);  //이름 검색 - 정지우
 void searchPrice(Cafe *c[],int count); //가격 검색 - 정지우
 void searchTaste(Cafe *c[],int count); //맛 검색 - 정지우
 void searchType(Cafe *c[],int count); //종류 검색 - 정지우
-int selectMenu(); //메뉴선택 - 정승민
+int selectMenu(){
+    int menu;
+    printf("1. 조회\n");
+    printf("2. 추가\n");
+    printf("3. 수정\n");
+    printf("4. 삭제\n");
+    printf("5. 파일저장(구현되지 않음)\n");
+    printf("6. 이름검색(구현되지 않음)\n");
+    printf("7. 설명검색(구현되지 않음)\n");
+    printf("8. 종류검색(구현되지 않음)\n");
+    printf("9. 가격검색(구현되지 않음)\n");
+    printf("0. 종료\n\n");
+    printf("=> 원하는 메뉴는? ");
+    scanf("%d", &menu);
+    return menu;
+} //메뉴선택 - 정승민
 int createProduct(Cafe *c){
     printf("상품의 이름은? ");
     getchar();
